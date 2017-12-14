39906a4f8d40d84f44c091dc8acb9e03b00fc97b
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-12-14 14:15:31 +0000

    Remove log

--- src/CredstashToEnv.js
+++ src/CredstashToEnv.js
@@ -63,8 +63,6 @@ export default class CredstashToEnv {
           params = assign({}, await handler.exec(params));
         }
 
-        console.log(params);
-
         await writeEnvFile(params);
         process.exit();
       })();
