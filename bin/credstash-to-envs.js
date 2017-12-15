9749c67123a42db71f6a3e2b147a24256298b36c
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-12-15 15:56:37 +0000

    Remove babel-polyfill duplication

--- bin/credstash-to-envs.js
+++ bin/credstash-to-envs.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env ./node_modules/.bin/babel-node
 'use strict';
 
-require('babel-polyfill');
-
 var CredstashToEnv = require('../dist/CredstashToEnv');
 var cred = new CredstashToEnv.default({ projectName: 'full-monty' });
 cred.execute();
